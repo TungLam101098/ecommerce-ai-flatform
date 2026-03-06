688b938a5dd09971e8f6c63e4fc0e92597a41ff2
lam.nguyen <n.t.lam10101998@gmail.com>
2026-03-06 10:05:37 +0700

    setup typegen for sanity

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { sanityFetch } from "@/sanity/lib/live";
+import { ALL_CATEGORIES_QUERY } from "@/sanity/queries/categories";
 
 export default async function Home() {
-  const categories = await sanityFetch({
-    query: '*'
+  const { data: categories } = await sanityFetch({
+    query: ALL_CATEGORIES_QUERY
   })
   console.log(categories)
   return (
